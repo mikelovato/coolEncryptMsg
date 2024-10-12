4765f9368519f3cf30492cb5ab4e9a3aa8a83f02
KSWoon <108384121+KSWoon@users.noreply.github.com>
2024-10-12 09:18:18 +0800

    Update forms.py

--- EncryptMsg/forms.py
+++ EncryptMsg/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 
-
 class MessageForm(forms.Form):
     content = forms.CharField(widget=forms.Textarea)
     encryption_method = forms.ChoiceField(choices=[
         ('fernet', 'Fernet'),
-        ('symmetric', 'Symmetric')
+        ('aes_cfb', 'AES (CFB Mode)')  
     ])
