084149416d8182ea4765a00454f141950af9baff
KSWoon <108384121+KSWoon@users.noreply.github.com>
2024-10-18 21:26:52 +0800

    Update forms.py

--- EncryptMsg/forms.py
+++ EncryptMsg/forms.py
@@ -4,8 +4,8 @@ class MessageForm(forms.Form):
     content = forms.CharField(widget=forms.Textarea)
     encryption_method = forms.ChoiceField(choices=[
         ('fernet', 'Fernet'),
-        ('aes_cfb', 'AES (CFB Mode)'),
-        ('aes_ctr', 'AES (CTR Mode)'),
-        ('aes_gcm', 'AES (GCM Mode)'),
         ('chacha20_poly1305', 'ChaCha20-Poly1305'),
+        ('aes_cfb_256', 'AES (CFB Mode) - 256 Key size'),
+        ('aes_ctr_256', 'AES (CTR Mode) - 256 Key size'),
+        ('aes_gcm256', 'AES (GCM Mode)  - 256 Key size'),
     ])
