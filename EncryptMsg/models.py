d8681776ee79e1131f9bd6e171fac7eef56e2331
KSWoon <108384121+KSWoon@users.noreply.github.com>
2024-10-18 14:33:57 +0800

    Update models.py

--- EncryptMsg/models.py
+++ EncryptMsg/models.py
@@ -5,9 +5,15 @@ class Message(models.Model):
     encryption_method = models.CharField(max_length=50)  # Method used for encryption
     encrypted_content = models.TextField()  # Encrypted message content
     hashed_content_sha256 = models.CharField(max_length=64)  # SHA-256 hash (64 characters)
+    hashed_content_sha3 = models.CharField(max_length=64)  # SHA-3 hash (64 characters)
+    hashed_content_argon2 = models.TextField()  # Argon2 hash (variable length)
+    hashed_content_scrypt = models.TextField()  # Scrypt hash (variable length)
     hashed_content_bcrypt = models.TextField()  # bcrypt hash (variable length)
     encryption_time = models.DecimalField(max_digits=10, decimal_places=6)  # Time spent on encryption
     sha256_hash_time = models.DecimalField(max_digits=10, decimal_places=6)  # Time spent on SHA-256 hashing
+    sha3_hash_time = models.DecimalField(max_digits=10, decimal_places=6)  # Time spent on SHA-3 hashing
+    argon2_hash_time = models.DecimalField(max_digits=10, decimal_places=6)  # Time spent on Argon2 hashing
+    scrypt_hash_time = models.DecimalField(max_digits=10, decimal_places=6)  # Time spent on Scrypt hashing
     bcrypt_hash_time = models.DecimalField(max_digits=10, decimal_places=6)  # Time spent on bcrypt hashing
 
     def __str__(self):
